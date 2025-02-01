85a146046133d72e653a3e47e6f93d00d8a69895
Your Name <you@example.com>
2025-02-01 09:21:54 +0100

    two failed paradigms

---
+++ ip_based_access_control.js
@@ -0,0 +1,27 @@
+const redirect_map = { //NOTE coutries are represented with the ISO 3166-1 alpha-2
+	"MA": "https://google.com"
+}
+const ipinfo_api_key = "";
+
+document.documentElement.style.display = "none";
+
+(async () => {
+	try {
+		const res = await fetch(`https://ipinfo.io/json?token=${ipinfo_api_key}`)
+		if (!res.ok) throw new Error("failed to fetch")
+
+		const data = await res.json()
+		const {country} = data
+		
+		// window.location = country in redirect_map ? redirect_map[country] : "./page.html" //NOTE the index/page paradigm
+
+		if (country in redirect_map) {
+			window.location = redirect_map[country] //NOTE the single page (index_page) paradigm
+		}
+	} catch (err) {
+		console.error(err);
+	}
+	finally {
+		document.documentElement.style.display = "";
+	}
+})();
